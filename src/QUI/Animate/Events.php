c106621b7f88087590d5d405c691222521254da3
Henning Leutz <leutz@pcsg.de>
2015-07-30 09:20:12 +0200

    template events

---
+++ src/QUI/Animate/Events.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file contains \QUI\Animate\Events
+ */
+
+namespace QUI\Animate;
+
+use QUI;
+
+/**
+ * Class Events
+ *
+ * @package quiqqer/animate
+ */
+class Events
+{
+    /**
+     * Event : on template get header
+     *
+     * @param QUI\Template $Site
+     */
+    static function onTemplateGetHeader($Template)
+    {
+echo 1;
+    }
+}
